4e0c14de733bcb713ed87bb879d6ab91da7893eb
Daniel Resúa Melón <danielresua@teis.intranet>
2024-10-09 16:24:03 +0200

    adding ejercicio_tipo_examen

---
+++ tema1/ejercicio_tipo_examen.py
@@ -0,0 +1,22 @@
+# Realizar un algoritmo que pinte la letra U por pantalla hecha con asteriscos.
+# El programa pedirá la altura. 
+# Fíjate que el programa inserta un espacio y pinta dos asteriscos menos en la base
+# para simular la curvatura de la letra.
+# Es importante indicar al usuario que la altura mínima debe ser de 4.
+# 
+# Ejemplo 1:
+# Introduzca la altura de la U: 5
+# 
+# *     *
+# *     *
+# *     *
+# *     *
+#   *** 
+# Ejemplo 2:
+
+# Introduzca la altura de la U: 4
+
+# *    *
+# *    *
+# *    *
+#   **
